94d9a5bd2eed7ca029ee92d0d6dfbaa27db8caea
Arnaldo Russo <arnaldorusso@gmail.com>
2015-03-22 22:27:27 -0300

    including blueprints

--- pytient/__init__.py
+++ pytient/__init__.py
@@ -9,6 +9,12 @@ app.config["SECRET_KEY"] = "S333cr3t"
 
 db = MongoEngine(app)
 
+def register_blueprints(app):
+    # Prevents circular imports
+    from pytient.views import posts
+    app.register_blueprint(posts)
+
+register_blueprints(app)
 
 if __name__ == '__main__':
         app.run()
