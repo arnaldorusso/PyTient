19ac121e9617bbda2108c3c7e7708fffa3b11511
Arnaldo Russo <arnaldorusso@gmail.com>
2015-08-04 08:38:09 -0300

    timestamp recorded

--- pytient/views.py
+++ pytient/views.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, url_for
 from models import Register_patient
 from forms import PatientForm
 from pytient_app import app, db
+import datetime
 
 @app.route("/")
 def index():
@@ -13,8 +14,13 @@ def index():
 def newpatient():
     form = PatientForm()
     if form.validate_on_submit():
-        cadastro = Register_patient()
-        form.populate_obj(cadastro)
+        cadastro = Register_patient(
+            form.nome.data,
+            form.rg.data,
+            form.nascimento.data,
+            datetime.datetime.now()
+            )
+        # form.populate_obj(cadastro)
         db.session.add(cadastro)
         db.session.commit()
     return redirect(url_for('index'))
