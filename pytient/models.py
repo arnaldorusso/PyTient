d2b21f71c1b662d1cb2c97f1f217758055f4a0b8
Arnaldo Russo <arnaldorusso@gmail.com>
2015-08-04 08:37:39 -0300

    updating models

--- pytient/models.py
+++ pytient/models.py
@@ -4,10 +4,10 @@ import datetime
 class Register_patient(db.Model):
     """Register fields"""
     id = db.Column(db.Integer, primary_key=True)
-    datetime = db.Column(db.DateTime(datetime.datetime.now()))
     nome = db.Column(db.String(40))
     rg = db.Column(db.String(12))
     nascimento = db.Column(db.String(12))
+    timestamp = db.Column(db.DateTime, nullable=False)  # (datetime.datetime.now()), nullable=False)
     # endereco = db.Column(db.String(63))
     # numero = db.Column(db.String(12))
     # complemento = db.Column(db.String(25))
@@ -16,6 +16,10 @@ class Register_patient(db.Model):
     # exame = db.Column(db.String(25))
     # valor = db.Column(db.String(12))
 
-    # def __init__(self, nome, rg):
-    #     self.nome = nome
-    #     self.rg = rg
+    def __init__(self, nome, rg, nascimento, timestamp):
+        # if timestamp is None:
+        #     timestamp = datetime.datetime.now()
+        self.nome = nome
+        self.rg = rg
+        self.nascimento = nascimento
+        self.timestamp = timestamp
